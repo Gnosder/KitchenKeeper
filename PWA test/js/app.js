d12e6dab98a5cefc3fe517132c72c5161672d50e
Patrick Plaisted <patrick.plaisted@gmail.com>
2021-03-25 19:15:07 -0700

    pwa service worker and finish

--- PWA test/js/app.js
+++ PWA test/js/app.js
@@ -27,3 +27,12 @@ const showCoffees = () => {
 }
 
 document.addEventListener("DOMContentLoaded", showCoffees)
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("Service worker registered"))
+            .catch(err => console.log("Service worker not registered", err))
+    })
+}
